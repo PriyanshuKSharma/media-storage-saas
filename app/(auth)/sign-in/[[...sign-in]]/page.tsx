fd71d48857e4ec7407bca9996284ed2d793967e7
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-03-28 13:14:41 +0530

    Centred sign-in and sign-out

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <div className="w-full max-w-md -mt-16">
+        <SignIn />
+      </div>
+    </div>
+  )
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <div className="w-full max-w-md -mt-16">
+        <SignUp />
+      </div>
+    </div>
+  )
 }
